b43de5a569472892284e2f0b526c0144f4beb3cf
fwcd <fwcdmail@gmail.com>
2018-05-27 13:46:14 +0200

    More cleanup in main.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,25 +1,12 @@
-import { BrowserWindow, app } from "electron";
 import * as path from "path";
-import { LOG, LogLevel, STBlock, STContext, STJSObject, STLoader, STMessageParameter, STObject, STString } from "smallballoon";
-
-let stLoader = new STLoader();
-let stContext = STContext.create();
-stContext.setVariableLocally("appPath", new STString(path.join(__dirname, "../")));
+import { LOG, LogLevel, STContext, STLoader, STString } from "smallballoon";
 
 LOG.level = LogLevel.Debug;
 
+let stContext = STContext.create();
+stContext.setVariableLocally("appPath", new STString(path.join(__dirname, "../")));
+
 LOG.debug("Loading Smalltalk application...");
-let stApp = stLoader.createASTFromFile("src/smalltalk/main.st");
+let stApp = new STLoader().createASTFromFile("src/smalltalk/main.st");
 LOG.debug("Running Smalltalk application...");
 stApp.runWith(stContext);
-
-function callSmalltalk(name: string, ...args: STMessageParameter[]): STObject {
-	return stContext.getVariable(name).expect(STBlock).evaluateWith([], args);
-}
-
-function instantiateWindow(width: number, height: number): Electron.BrowserWindow {
-	return new BrowserWindow({
-		height: height,
-		width: width
-	});
-}
