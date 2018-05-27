c495172e3f4406aade60691ca1233bac595839e8
fwcd <fwcdmail@gmail.com>
2018-05-27 13:43:32 +0200

    Moved event handling to Smalltalk

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,9 +2,9 @@ import { BrowserWindow, app } from "electron";
 import * as path from "path";
 import { LOG, LogLevel, STBlock, STContext, STJSObject, STLoader, STMessageParameter, STObject, STString } from "smallballoon";
 
-let mainWindow: Electron.BrowserWindow;
 let stLoader = new STLoader();
 let stContext = STContext.create();
+stContext.setVariableLocally("appPath", new STString(path.join(__dirname, "../")));
 
 LOG.level = LogLevel.Debug;
 
@@ -23,31 +23,3 @@ function instantiateWindow(width: number, height: number): Electron.BrowserWindo
 		width: width
 	});
 }
-
-function createWindow() {
-	LOG.debug("Creating browser window through Smalltalk...");
-	mainWindow = callSmalltalk("createBrowserWindow").expect(STJSObject).getObject();
-
-	LOG.debug("Initializing browser window through Smalltalk...");
-	callSmalltalk("initialize",
-		{label: "mainWindow", value: new STJSObject(stContext, mainWindow)},
-		{label: "appPath", value: new STString(path.join(__dirname, "../"))}
-	);
-
-	// Emitted when the window is closed.
-	mainWindow.on("closed", () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
-}
-
-app.on("ready", createWindow);
-app.on("activate", () => {
-	// On OS X it"s common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow();
-	}
-});
